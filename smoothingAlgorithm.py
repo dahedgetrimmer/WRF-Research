faabecd487ec0178645816e557883d1bd5ea3048
Jared Revier <119819370+dahedgetrimmer@users.noreply.github.com>
2024-02-25 16:40:57 -0600

    Update smoothingAlgorithm.py

--- smoothingAlgorithm.py
+++ smoothingAlgorithm.py
@@ -3,6 +3,9 @@
 """
 Created on Fri Feb  9 14:18:08 2024
 
+This script extracts/modifies the elevation (HGT_M) variable
+from a WRF Preprocessing System generated netCDF file.
+
 @author: jrevier
 """
 
@@ -11,45 +14,45 @@ import netCDF4 as nc
 import scipy.ndimage
 import numpy as np
 
-file = '/home/jrevier/WRF/WPS/geo_em.d01.nc'
-newfile = '/home/jrevier/WRF/WPS/og_met_em/geo_em_copy.nc'
+#set paths
+file = 'PATH_TO_ORIGINAL_netCDF_FILE'
+newfile = 'PATH_TO_EDITED_netCDF_FILE'
 
+#create a copy as a precaution
 shutil.copy2(file, newfile)
 
+#read in dataset
 ds = nc.Dataset(file, 'r+')
 
-
+#define the bounds of the edited area
 x1 = 365
 x2 = 425
 y1 = 325
 y2 = 415
 
 
-
+#assign the heights of the edit domain
 hgt = ds.variables['HGT_M'][0, y1:y2, x1:x2]
 
+#use numpy mean as an initial smooth
+#replace height values in the netcdf file
 hgt[:,:] = np.mean(hgt)-150
 ds.variables['HGT_M'][:, y1:y2, x1:x2] = hgt[:,:]
 
+#extend and reassign the edit domain for second pass with Gaussian filter
 x1 -= 5
 x2 += 30
 y1 -= 10
 y2 += 10
 hgt = ds.variables['HGT_M'][0, y1:y2, x1:x2]
-sub_slope = ds.variables['VAR_SSO'][0, y1:y2, x1:x2]
 
-sub_slope[:,:] = np.zeros( np.shape(sub_slope) )
 
-# Apply Gaussian filter
+# Apply Gaussian filter in 2 directions
 hgt_filtered = scipy.ndimage.gaussian_filter1d(hgt, sigma =25, axis=0)
 hgt_filtered2 = scipy.ndimage.gaussian_filter1d(hgt_filtered, sigma=10, axis=-1)
 
+#replace the height values in the netCDF file
 ds.variables['HGT_M'][:, y1:y2, x1:x2] = hgt_filtered2[:,:]
-ds.variables['VAR_SSO'][: , y1:y2, x1:x2] = sub_slope[:, :]
 
-
-
-
-#print(hgt_filtered)
 
 ds.close()
